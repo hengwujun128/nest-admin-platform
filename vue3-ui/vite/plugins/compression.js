f932a612c5c7c0a886ec5c31fa13edb5873c99b3
张泽全 <hengwujun128@gmail.com>
2024-12-20 09:56:52 +0800

    feat: change system title

--- vue3-ui/vite/plugins/compression.js
+++ vue3-ui/vite/plugins/compression.js
@@ -1,28 +1,37 @@
+/*
+ * @Author: 张泽全 hengwujun128@gmail.com
+ * @Date: 2024-09-30 16:55:30
+ * @LastEditors: 张泽全 hengwujun128@gmail.com
+ * @LastEditTime: 2024-12-20 09:56:09
+ * @Description:
+ * @FilePath: /meimei-prisma-vue3/vue3-ui/vite/plugins/compression.js
+ */
 import compression from 'vite-plugin-compression'
 
 export default function createCompression(env) {
-    const { VITE_BUILD_COMPRESS } = env
-    const plugin = []
-    if (VITE_BUILD_COMPRESS) {
-        const compressList = VITE_BUILD_COMPRESS.split(',')
-        if (compressList.includes('gzip')) {
-            // http://doc.ruoyi.vip/ruoyi-vue/other/faq.html#使用gzip解压缩静态文件
-            plugin.push(
-                compression({
-                    ext: '.gz',
-                    deleteOriginFile: false
-                })
-            )
-        }
-        if (compressList.includes('brotli')) {
-            plugin.push(
-                compression({
-                    ext: '.br',
-                    algorithm: 'brotliCompress',
-                    deleteOriginFile: false
-                })
-            )
-        }
+  const { VITE_BUILD_COMPRESS } = env
+  const plugin = []
+  if (VITE_BUILD_COMPRESS) {
+    const compressList = VITE_BUILD_COMPRESS.split(',')
+    if (compressList.includes('gzip')) {
+      // http://doc.ruoyi.vip/ruoyi-vue/other/faq.html#使用gzip解压缩静态文件
+      plugin.push(
+        compression({
+          ext: '.gz',
+          deleteOriginFile: false,
+        })
+      )
     }
-    return plugin
+
+    if (compressList.includes('brotli')) {
+      plugin.push(
+        compression({
+          ext: '.br',
+          algorithm: 'brotliCompress',
+          deleteOriginFile: false,
+        })
+      )
+    }
+  }
+  return plugin
 }
